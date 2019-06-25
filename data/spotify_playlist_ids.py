e3c29f9c87db048b6f574bd4c7cedc5cfa296398
paigemoody <moodypaige.e@gmail.com>
2019-06-25 16:09:08 +0000

    WIP get country playlist ids

---
+++ data/spotify_playlist_ids.py
@@ -0,0 +1,12 @@
+spotify_country_playist_ids = {
+"Asia" : { "Japan" : "", "Israel": "", "Hong Kong": "", "Indonesia": "", "Malaysia": "", "Philippines": "", "Singapore": "", "Taiwan": "", "Thailand": "", "Vietnam": "", "India": ""},
+"Europe" :  {"Andorra": "", "Austria": "", "Belgium": "", "Bulgaria": "", "Cyprus": "", "Czech Republic": "", "Denmark": "", "Estonia": "", "Finland": "", "France": "", "Germany": "", "Greece": "", "Hungary": "", "Iceland": "", "Ireland": "", "Italy": "", "Latvia": "", "Liechtenstein": "", "Lithuania": "", "Luxembourg": "", "Malta": "", "Monaco": "", "Netherlands": "", "Norway": "", "Poland": "", "Portugal": "", "Romania": "", "Slovakia": "", "Spain": "", "Sweden": "", "Switzerland": "", "Turkey": "", "United Kingdom}": ""},
+"Latin America and  Caribbean ": { "Argentina": "", "Bolivia": "", "Brazil": "", "Chile": "", "Colombia": "", "Costa Rica": "", "Dominican Republic": "", "Ecuador": "", "El Salvador": "", "Guatemala": "", "Honduras": "", "Mexico": "", "Nicaragua": "", "Panama": "", "Paraguay": "", "Peru": "", "Uruguay": ""},
+"North America":  {"Canada": "", "United States" : "37i9dQZEVXbMDoHDwVN2tF"},
+"Africa" : {"Algeria": "", "Egypt": "", "Morocco": "", "South Africa": "", "Tunisia": ""},
+"Oceania" : {"Australia": "", "New Zealand": ""},
+"Middle East" : {"Bahrain": "", "Jordan": "", "Kuwait": "", "Lebanon": "", "Oman": "", "Palestine": "", "Qatar": "", "Saudi Arabia": "", "United Arab Emirates": ""}
+}
+
+for region in spotify_country_playist_ids:
+    print(spotify_country_playist_ids[region])
